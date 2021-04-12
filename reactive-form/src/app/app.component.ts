7bde16ccaa7ba75eed1df76a2721a0681a95ab82
Rishabh Tripathi <47689841+vegito27@users.noreply.github.com>
2021-04-12 23:37:00 +0530

    simple reactive form

---
+++ reactive-form/src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormGroup,FormControl  } from '@angular/forms';
+import {FormBuilder,Validators } from '@angular/forms'
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private fb:FormBuilder){}
+
+
+  get userName(){
+    return this.registrationForm.get('userName')
+  }
+
+    registrationForm=this.fb.group({
+
+      userName:['',[Validators.required,Validators.minLength(3)]],
+      password:[''],
+      confirmPassword:[''],
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        postalCode:['']
+      })
+
+
+    })
+
+  
+
+  // registrationForm=new FormGroup({
+
+  //     userName:new FormControl('Rishabh'),
+  //     Password:new FormControl('Password'),
+  //     confirmPassword:new FormControl(''),
+  //     address: new FormGroup({
+  //       city: new FormControl(''),
+  //       state: new FormControl(''),
+  //       postalCode: new FormControl('')
+  //     }) 
+
+  // }) 
+
+  // loadAPIData(){
+  //   this.registrationForm.setValue({
+  //     userName:"Bruce",
+  //     Password:'test',
+  //     confirmPassword:'test',
+  //     address:{
+  //       city:'City',
+  //       state:'State',
+  //       postalCode:'123456'
+  //     }
+  //   })
+  // }
+}
